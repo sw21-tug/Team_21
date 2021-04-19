8b01421286ce4e9f4bb2e02624e43d4a59ce7652
Michael Streibl <michael.streibl@student.tugraz.at>
2021-04-19 19:55:48 +0200

    GMP-14 Created Login and Register Design and the corresponding linking [MS,SH]

---
+++ GetMyApp/app/src/main/java/com/example/getmyapp/ui/login/RegisterFragment.kt
@@ -0,0 +1,29 @@
+package com.example.getmyapp.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.getmyapp.R
+
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_register, container, false)
+        //val textView: TextView = root.findViewById(R.id.text_login)
+//        loginViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+
+
+        return root
+    }
+}
