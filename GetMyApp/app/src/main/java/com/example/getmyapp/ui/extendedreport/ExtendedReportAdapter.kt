12bfb5c04f95bd26fc123a028c7b50fd4fb74e0b
Denis Tomic <d.tomic@student.tugraz.at>
2021-05-26 12:17:04 +0200

    GMP-3 Implementation of Extended Pet Report [TD,BN]

--- GetMyApp/app/src/main/java/com/example/getmyapp/ui/extendedreport/ExtendedReportAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.getmyapp.ui.extendedreport
-
-class ExtendedReportAdapter {
-}
--- GetMyApp/app/src/main/java/com/example/getmyapp/ui/extendedreport/ExtendedReportFragment.kt
+++ GetMyApp/app/src/main/java/com/example/getmyapp/ui/extendedreport/ExtendedReportFragment.kt
@@ -1,6 +1,42 @@
 package com.example.getmyapp.ui.extendedreport
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.getmyapp.R
+import com.example.getmyapp.ui.home.HomeViewModel
 
 class ExtendedReportFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_extendedreport, container, false)
+        val nameTextView : TextView = root.findViewById(R.id.extendedReportPetNameInfoTextView)
+        val speciesTextView : TextView = root.findViewById(R.id.extendedReportAnimalSpeciesInfoTextView)
+        val breedTextView : TextView = root.findViewById(R.id.extendedReportBreedInfoTextView)
+        val colorTextView : TextView = root.findViewById(R.id.extendedReportColourInfoTextView)
+        val ageTextView : TextView = root.findViewById(R.id.extendedReportAgeInfoTextView)
+        val regionTextView : TextView = root.findViewById(R.id.extendedReportRegionInfoTextView)
+        val genderTextView : TextView = root.findViewById(R.id.extendedReportGenderTextView)
+        val lastSeenTextView : TextView = root.findViewById(R.id.extendedReportLastSeenInfoTextView)
+        val chipNrTextView : TextView = root.findViewById(R.id.extendedReportChipNumberInfoTextView)
+        nameTextView.text = arguments?.getString("name")
+        speciesTextView.text = arguments?.getString("species")
+        breedTextView.text = arguments?.getString("breed")
+        colorTextView.text = arguments?.getString("color")
+        ageTextView.text = arguments?.getString("age")
+        regionTextView.text = arguments?.getString("region")
+        genderTextView.text = arguments?.getString("gender")
+        lastSeenTextView.text = arguments?.getString("lastSeen")
+        chipNrTextView.text = arguments?.getString("chipNo")
+        return root
+    }
 }
